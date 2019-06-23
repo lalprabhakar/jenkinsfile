3e634b4c403a21dbdbd38faadf26590a5751c0e4
prabhakar <prabhakar.lal@mygurukulam.org>
2019-06-23 05:36:40 -0400

    This is hardcoded of path

--- pipeline.sh
+++ pipeline.sh
@@ -4,7 +4,7 @@ pipeline{
         stage('Clone of Git'){
             steps {
                    sh 'git clone git@github.com:opstree/spring3hibernate.git'
-                   sh 'cd spring3hibernate'
+                   sh 'cd /var/lib/jenkins/workspace/pipelinework/spring3hibernate'
                    sh 'pwd'
                    sh 'mvn install'
             }
