aed747baeca9970be98a1844e60ffe9901e0dfa5
prabhakar <prabhakar.lal@mygurukulam.org>
2019-06-23 05:22:51 -0400

    Sipmle Cloning of Git

--- pipeline.sh
+++ pipeline.sh
@@ -3,7 +3,7 @@ pipeline{
     stages{
         stage('Clone of Git'){
             steps {
-                   sh 'rm -rf /var/lib/jenkins/workspace/pipelinework*;git clone git@github.com:opstree/spring3hibernate.git'
+                   sh 'git clone git@github.com:opstree/spring3hibernate.git'
                    sh 'cd spring3hibernate'
                    sh 'mvn install'
             }
