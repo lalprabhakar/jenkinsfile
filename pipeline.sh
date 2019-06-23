cf45f588aef84a681a6de273aa91fd0920bbe450
prabhakar <prabhakar.lal@mygurukulam.org>
2019-06-23 08:11:43 -0400

    This is for publish report

--- pipeline.sh
+++ pipeline.sh
@@ -46,6 +46,15 @@ pipeline{
             }
     }
 
+  stage('Publish Report'){
+            steps {
+                  cobertura autoUpdateHealth: false, autoUpdateStability: false, coberturaReportFile: '**/target/site/cobertura/coverage.xml', conditionalCoverageTargets: '70, 0, 0', failUnhealthy: false, failUnstable: false, lineCoverageTargets: '80, 0, 0', maxNumberOfBuilds: 0, methodCoverageTargets: '80, 0, 0', onlyStable: false, sourceEncoding: 'ASCII', zoomCoverageChart: false
+                  checkstyle canComputeNew: false, defaultEncoding: '', healthy: '', pattern: '**/checkstyle-result.xml', unHealthy: ''
+                  findbugs canComputeNew: false, defaultEncoding: '', excludePattern: '', healthy: '', includePattern: '', pattern: '**/findbugsXml.xml', unHealthy: ''
+ 
+
+            }
+    }
 
 
     
