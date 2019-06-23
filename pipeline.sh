f7349322aca0119496a3962218a9f3a832d0dd9e
prabhakar <prabhakar.lal@mygurukulam.org>
2019-06-23 07:30:14 -0400

    Updating User Input

--- pipeline.sh
+++ pipeline.sh
@@ -3,21 +3,28 @@ pipeline{
     stages{
         stage('Clone of Git'){
             steps {
-                   sh 'git clone git@github.com:opstree/spring3hibernate.git'
+                   sh 'rm -rf spring3hibernate;git clone git@github.com:opstree/spring3hibernate.git'
                    sh 'cd spring3hibernate; mvn install'
             }
 
          }
          stage('Email Notification'){
             steps {
-                  mail bcc: '', body: 'Hello Bulid started', cc: '', from: '', replyTo: '', subject: 'Going to start build', to: 'prabhakarlal20@gmail.com'
+                  mail bcc: '', body: 'Hello Bulid started', cc: '', from: '', replyTo: '', subject: 'Prabhakar to start build', to: 'prabhakarlal20@gmail.com'
                    
             }
 
          }
          stage('Slack Notification'){
             steps {
-                  slackSend channel: 'ot-ninja-batch5', color: 'Green', iconEmoji: '', message: 'Going to start build', tokenCredentialId: 'plal', username: ''
+                  slackSend channel: 'ot-ninja-batch5', color: 'Green', iconEmoji: '', message: 'Prabhakar to start build', tokenCredentialId: 'plal', username: ''
+            }
+
+         }
+
+         stage('Input Process'){
+            steps {
+                input message: 'Only for admin User', submitter: 'proceed'
             }
 
          }
