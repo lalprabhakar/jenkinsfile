92a083a266a66cbef682a184b4bad0e36088ff4f
prabhakar <prabhakar.lal@mygurukulam.org>
2019-06-23 06:24:59 -0400

    This is for email notification

--- pipeline.sh
+++ pipeline.sh
@@ -3,12 +3,20 @@ pipeline{
     stages{
         stage('Clone of Git'){
             steps {
+                   sh 'rm -rf /var/lib/jenkins/workspace/pipelinework*'
                    sh 'git clone git@github.com:opstree/spring3hibernate.git'
                    sh 'cd spring3hibernate; mvn install'
             }
 
          }
-        
+         stage('Email Notification'){
+            steps {
+                  mail bcc: '', body: 'Hello Bulid started', cc: '', from: '', replyTo: '', subject: 'Going to start build', to: 'prabhakarlal20@gmail.com'
+                   
+            }
+
+         }
+
     
 
     }
